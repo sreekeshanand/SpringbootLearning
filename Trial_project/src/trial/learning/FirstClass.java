00ef118b4be9f624cb36444c21f13e67af95f728
sreek <sreek@192.168.1.16>
2018-08-06 13:54:12 -0230

    Workaround

--- Trial_project/src/trial/learning/FirstClass.java
+++ Trial_project/src/trial/learning/FirstClass.java
@@ -1,9 +1,13 @@
 package trial.learning;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FirstClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		List<Integer> age = new ArrayList<Integer>();
 
 	}
 
